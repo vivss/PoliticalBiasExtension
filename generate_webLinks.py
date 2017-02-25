8127e4344093ec29e0eb8fe041e1dfea7039a555
Vivian Li <vli4@u.rochester.edu>
2017-02-25 17:30:41 -0500

    Generated data files with urls for media bias

---
+++ generate_webLinks.py
@@ -0,0 +1,35 @@
+import sys
+
+f = open('Input.txt', "r")
+newfile = open('output.txt', "w")
+
+for line in f:
+	newcontent ='' # where I start storing url
+	#stores count for .com letter
+	countMatches = 0
+	for letter in line:
+		if countMatches == 4:
+			if letter == '/':
+				break
+			if letter != '-':
+				newcontent += letter
+		elif letter == 'c':
+			countMatches+=1
+		elif letter == 'o' and countMatches == 1:
+			countMatches+=1
+		elif letter == 'm' and countMatches == 2:
+			countMatches+=1
+		elif letter == '/' and countMatches == 3:
+			countMatches+=1
+		else:
+			countMatches = 0
+
+	if newcontent == '':
+		newcontent = line
+	sys.stdout.write(newcontent+".com\n")
+	#print newcontent
+	newfile.write(newcontent+".com\n")
+
+
+f.close()
+newfile.close()
